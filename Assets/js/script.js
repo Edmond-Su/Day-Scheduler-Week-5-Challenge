776745ba2bd940108c82d39ed032d3174b6df823
Edmond Su <133119772+deverstater07@users.noreply.github.com>
2023-06-27 16:51:17 +1000

    Coded all features and added comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,22 +2,76 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  var dayListEl = $('#dayListContainer');
+
+  function handleSaveAppointment(event) {
+    console.log("CLICK")
+    var btnClicked = $(event.target)
+    // console.log(btnClicked)
+    var btnParentEl = btnClicked.closest('div')
+    var btnParentId = btnParentEl.attr('id')
+    // console.log(btnParentEl)
+    // // console.log(btnParentId)
+    var eventTextEl = btnParentEl.children('.description')
+    // console.log(eventTextEl)
+    var textToSave = eventTextEl.val()
+    // console.log(textToSave)
+    if (textToSave != " ") {
+      localStorage.setItem(btnParentId, textToSave)
+      var savedEl = $('<p>')
+      savedEl.text('Appointment saved to local storage')
+      savedEl.css('text-align', 'center')
+      dayListEl.prepend(savedEl)
+      setTimeout(function () {
+        savedEl.remove()
+      }, 3000)
+    }
+
+  }
+
+  dayListEl.on('click', '.saveBtn', handleSaveAppointment)
+
+  // Getting current hour and saving it to a variable.
+  var currentTime = dayjs().format('HH');
+  // Looping through all the hourly time blocks and applying a class to them.
+  for (var i = 9; i < 18; i++) {
+    //assigning a variable to the time block element with the class hour-'i' 
+    var hourEl = $('#hour-' + i);
+    // Checking if the current hour is less than, greater than or equal to the current hour and applying a class depending which it is.
+    if (i < currentTime) {
+      hourEl.addClass('past');
+    } else if (i > currentTime) {
+      hourEl.addClass('future');
+    } else {
+      hourEl.addClass('present');
+    };
+  };
+
+  //Function that checks local storage for saved appointments
+  function readSavedAppoinments() {
+    for (var i = 9; i < 18; i++) {
+      var hourEl = $('#hour-' + i);
+      var textAreaEl = hourEl.children('.description')
+      var savedEvent = localStorage.getItem('hour-' + i)
+      if (savedEvent) {
+        console.log(savedEvent)
+        textAreaEl.text(savedEvent)
+      }
+
+
+
+    }
+  }
+  readSavedAppoinments()
+
+  //code to display the current date in the header of the page.
+  var today = dayjs().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(today);
 });
